b6ff2db74132a1331155bcac2de2c6f3055a2e3c
Jeff Wu <jeffywu@pm.me>
2021-10-11 04:18:49 -0700

    adding more encoders

--- contracts/lib/EncodeDecode.sol
+++ contracts/lib/EncodeDecode.sol
@@ -2,6 +2,7 @@
 pragma solidity >0.7.0;
 
 import "./Constants.sol";
+import "./Types.sol";
 
 library EncodeDecode {
 
@@ -38,10 +39,99 @@ library EncodeDecode {
             );
     }
 
-    // function encodeLendTrade()
-    // function encodeBorrowTrade()
-    // function encodeAddLiquidity()
-    // function encodeRemoveLiquidity()
-    // function encodePurchaseNTokenResidual()
-    // function encodeSettleCashDebt()
+    function encodeLendTrade(
+        uint8 marketIndex,
+        uint88 fCashAmount,
+        uint32 minImpliedRate
+    ) internal pure returns (bytes32) {
+        return
+            bytes32(
+                uint256(
+                    (uint8(TradeActionType.Lend) << 248) |
+                        (marketIndex << 240) |
+                        (fCashAmount << 152) |
+                        (minImpliedRate << 120)
+                )
+            );
+    }
+
+    function encodeBorrowTrade(
+        uint8 marketIndex,
+        uint88 fCashAmount,
+        uint32 maxImpliedRate
+    ) internal pure returns (bytes32) {
+        return
+            bytes32(
+                uint256(
+                    (uint8(TradeActionType.Borrow) << 248) |
+                        (marketIndex << 240) |
+                        (fCashAmount << 152) |
+                        (maxImpliedRate << 120)
+                )
+            );
+    }
+
+    function encodeAddLiquidity(
+        uint8 marketIndex,
+        uint88 assetCashAmount,
+        uint32 minImpliedRate,
+        uint32 maxImpliedRate
+    ) internal pure returns (bytes32) {
+        return
+            bytes32(
+                uint256(
+                    (uint8(TradeActionType.AddLiquidity) << 248) |
+                        (marketIndex << 240) |
+                        (assetCashAmount << 152) |
+                        (minImpliedRate << 120) |
+                        (maxImpliedRate << 88)
+                )
+            );
+    }
+
+    function encodeRemoveLiquidity(
+        uint8 marketIndex,
+        uint88 tokenAmount,
+        uint32 minImpliedRate,
+        uint32 maxImpliedRate
+    ) internal pure returns (bytes32) {
+        return
+            bytes32(
+                uint256(
+                    (uint8(TradeActionType.RemoveLiquidity) << 248) |
+                        (marketIndex << 240) |
+                        (tokenAmount << 152) |
+                        (minImpliedRate << 120) |
+                        (maxImpliedRate << 88)
+                )
+            );
+    }
+
+    function encodePurchaseNTokenResidual(
+        uint32 maturity,
+        int88 fCashResidualAmount
+    ) internal pure returns (bytes32) {
+        return
+            bytes32(
+                uint256(
+                    (uint8(TradeActionType.PurchaseNTokenResidual) << 248) |
+                        (maturity << 216) |
+                        (uint256(fCashResidualAmount) << 128)
+                )
+            );
+    }
+
+    function encodeSettleCashDebt(
+        address counterparty,
+        int88 fCashAmountToSettle
+    ) internal pure returns (bytes32) {
+        return
+            bytes32(
+                uint256(
+                    (uint8(TradeActionType.SettleCashDebt) << 248) |
+                        (uint256(counterparty) << 88) |
+                        (uint256(fCashAmountToSettle))
+                )
+            );
+    }
 }
