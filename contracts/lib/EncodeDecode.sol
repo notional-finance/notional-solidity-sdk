89569b228467c628c5890cb8040e722126a1ef4d
Jeff Wu <jeffywu@pm.me>
2021-10-11 04:07:09 -0700

    adding erc1155 encoders

--- contracts/lib/EncodeDecode.sol
+++ contracts/lib/EncodeDecode.sol
@@ -1,29 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.7.0;
 
+import "./Constants.sol";
+
 library EncodeDecode {
 
-    // function encodeERC1155ID()
-    // function decodeERC1155ID()
+    /// @notice Decodes asset ids
+    function decodeERC1155Id(uint256 id)
+        internal
+        pure
+        returns (
+            uint256 currencyId,
+            uint256 maturity,
+            uint256 assetType
+        )
+    {
+        assetType = uint8(id);
+        maturity = uint40(id >> 8);
+        currencyId = uint16(id >> 48);
+    }
+
+    /// @notice Encodes asset ids
+    function encodeERC1155Id(
+        uint256 currencyId,
+        uint256 maturity,
+        uint256 assetType
+    ) internal pure returns (uint256) {
+        require(currencyId <= Constants.MAX_CURRENCIES);
+        require(maturity <= type(uint40).max);
+        require(assetType <= Constants.MAX_LIQUIDITY_TOKEN_INDEX);
+
+        return
+            uint256(
+                (bytes32(uint256(uint16(currencyId))) << 48) |
+                (bytes32(uint256(uint40(maturity))) << 8) |
+                bytes32(uint256(uint8(assetType)))
+            );
+    }
 
     // function encodeLendTrade()
-    // function decodeLendTrade()
-
     // function encodeBorrowTrade()
-    // function decodeBorrowTrade()
-
     // function encodeAddLiquidity()
-    // function decodeAddLiquidity()
-
     // function encodeRemoveLiquidity()
-    // function decodeRemoveLiquidity()
-
     // function encodePurchaseNTokenResidual()
-    // function decodePurchaseNTokenResidual()
-
     // function encodeSettleCashDebt()
-    // function decodeSettleCashDebt()
-
-    // function getMarketIndex()
-    // function getMaturityFromMarketIndex()
 }
