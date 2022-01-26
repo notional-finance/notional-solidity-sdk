78a88f80c0b5e01deb80748eee11711d346df9d7
Jeff Wu <jeffywu@pm.me>
2022-01-26 06:01:15 -0800

    adding minting tests

--- tests/test_wrapped_fcash.py
+++ tests/test_wrapped_fcash.py
@@ -1,5 +1,6 @@
 import pytest
 import brownie
+from tests.helpers import get_balance_trade_action
 from brownie import Contract, WrappedfCash
 from brownie.network import chain
 from scripts.EnvironmentConfig import getEnvironment
@@ -26,8 +27,32 @@ def factory(WrappedfCashFactory, beacon, env):
 @pytest.fixture(autouse=True) 
 def wrapper(factory, env):
     markets = env.notional.getActiveMarkets(2)
-    factory.deployWrapper(2, markets[0][1])
-    return Contract.from_abi("Wrapper", computedAddress, WrappedfCash.abi)
+    txn = factory.deployWrapper(2, markets[0][1])
+    return Contract.from_abi("Wrapper", txn.events['WrapperDeployed']['wrapper'], WrappedfCash.abi)
+
+@pytest.fixture(autouse=True) 
+def lender(wrapper, env):
+    env.tokens["DAI"].approve(env.notional.address, 2**255-1, {'from': env.whales["DAI"]})
+    env.notional.batchBalanceAndTradeAction(
+        env.whales["DAI"],
+        [ 
+            get_balance_trade_action(
+                2,
+                "DepositUnderlying",
+                [{
+                    "tradeActionType": "Lend",
+                    "marketIndex": 1,
+                    "notional": 100_000e8,
+                    "minSlippage": 0
+                }],
+                depositActionAmount=100_000e18,
+                withdrawEntireCashBalance=True,
+                redeemToUnderlying=True,
+            )
+        ], { "from": env.whales["DAI"] }
+    )
+
+    return env.whales["DAI"]
 
 # Deploy and Upgrade
 
@@ -60,23 +85,62 @@ def test_cannot_deploy_invalid_maturity(factory, env):
     with brownie.reverts():
         factory.deployWrapper(2, markets[0][1] + 86400 * 720)
 
-def test_upgrade_wrapper(factory, env):
-    pass
-
 # Test Minting fCash
+def test_only_accepts_notional_v2(wrapper, beacon, lender, env):
+    impl = WrappedfCash.deploy(env.deployer.address, {"from": env.deployer})
 
-@pytest.mark.only
-def test_cannot_transfer_invalid_fcash(wrapper, env):
-    pass
+    # Change the address of notional on the beacon
+    beacon.upgradeTo(impl.address)
 
-def test_cannot_transfer_negative_fcash(wrapper, env):
-    pass
+    with brownie.reverts("Invalid caller"):
+        env.notional.safeTransferFrom(
+            lender.address,
+            wrapper.address,
+            wrapper.getfCashId(),
+            100_000e8,
+            "",
+            {"from": lender}
+        )
 
-def test_cannot_transfer_batch_fcash(wrapper, env):
-    pass
 
-def test_transfer_fcash(wrapper, env):
-    pass
+def test_cannot_transfer_invalid_fcash(lender, factory, env):
+    markets = env.notional.getActiveMarkets(2)
+    txn = factory.deployWrapper(2, markets[1][1])
+    wrapper = Contract.from_abi("Wrapper", txn.events['WrapperDeployed']['wrapper'], WrappedfCash.abi)
+    fCashId = env.notional.encodeToId(2, markets[0][1], 1)
+
+    with brownie.reverts():
+        env.notional.safeTransferFrom(
+            lender.address,
+            wrapper.address,
+            fCashId,
+            100_000e8,
+            "",
+            {"from": lender}
+        )
+
+def test_cannot_transfer_batch_fcash(wrapper, lender, env):
+    with brownie.reverts("Not accepted"):
+        env.notional.safeBatchTransferFrom(
+            lender.address,
+            wrapper.address,
+            [wrapper.getfCashId()],
+            [100_000e8],
+            "",
+            {"from": lender}
+        )
+
+def test_transfer_fcash(wrapper, lender, env):
+    env.notional.safeTransferFrom(
+        lender.address,
+        wrapper.address,
+        wrapper.getfCashId(),
+        100_000e8,
+        "",
+        {"from": lender}
+    )
+
+    assert wrapper.balanceOf(lender) == 100_000e8
 
 # Test Redeem fCash
 
