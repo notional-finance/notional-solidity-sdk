68ac2b4731d77326088c9d00d265868d09f21a20
Jeff Wu <jeffywu@pm.me>
2022-04-06 15:34:36 -0700

    some tests passing

--- scripts/EnvironmentConfig.py
+++ scripts/EnvironmentConfig.py
@@ -21,67 +21,37 @@ with open("abi/Notional.json") as a:
 
 ETH_ADDRESS = "0x0000000000000000000000000000000000000000"
 
+networks = {}
+
+with open("v2.mainnet.json", "r") as f:
+    networks["mainnet"] = json.load(f)
+
+with open("v2.kovan.json", "r") as f:
+    networks["kovan"] = json.load(f)
 
 class Environment:
-    def __init__(self) -> None:
+    def __init__(self, network) -> None:
+        self.network = network
+        addresses = networks[network]
+
         self.notional = Contract.from_abi(
-            "Notional", "0x1344a36a1b56144c3bc62e7757377d288fde0369", NotionalABI
+            "Notional", addresses["notional"], NotionalABI
         )
-        self.tokens = {
-            "NOTE": Contract.from_abi(
-                "ERC20", "0xCFEAead4947f0705A14ec42aC3D44129E1Ef3eD5", ERC20ABI
-            ),
-            "WETH": Contract.from_abi(
-                "ERC20", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", ERC20ABI
-            ),
-            "USDC": Contract.from_abi(
-                "ERC20", "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", ERC20ABI
-            ),
-            "DAI": Contract.from_abi(
-                "ERC20", "0x6b175474e89094c44da98b954eedeac495271d0f", ERC20ABI
-            ),
-            "WBTC": Contract.from_abi(
-                "ERC20", "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", ERC20ABI
-            ),
-            "LINK": Contract.from_abi(
-                "ERC20", "0x514910771af9ca656af840dff83e8264ecf986ca", ERC20ABI
-            ),
-            "COMP": Contract.from_abi(
-                "ERC20", "0xc00e94cb662c3520282e6f5717214004a7f26888", ERC20ABI
-            ),
-            "AAVE": Contract.from_abi(
-                "ERC20", "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9", ERC20ABI
-            ),
-            "cDAI": Contract.from_abi(
-                "cToken", "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643", cToken["abi"]
-            ),
-            "cUSDC": Contract.from_abi(
-                "cToken", "0x39aa39c021dfbae8fac545936693ac917d5e7563", cToken["abi"]
-            ),
-            "cWBTC": Contract.from_abi(
-                "cToken", "0xccf4429db6322d5c611ee964527d42e5d685dd6a", cToken["abi"]
-            ),
-            "cETH": Contract.from_abi(
-                "cEther", "0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5", cEther["abi"]
-            )
-        }
 
-        self.whales = {
-            "DAI_CONTRACT": accounts.at("0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7", force=True),
-            "DAI_EOA": accounts.at("0x1e3D6eAb4BCF24bcD04721caA11C478a2e59852D", force=True),
-            "ETH_CONTRACT": accounts.at("0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5", force=True), # cETH
-            "ETH_EOA": accounts.at("0xDA9dfA130Df4dE4673b89022EE50ff26f6EA73Cf", force=True), # Kraken
-            "USDC": accounts.at("0x0a59649758aa4d66e25f08dd01271e891fe52199", force=True),
-            "cDAI": accounts.at("0x33b890d6574172e93e58528cd99123a88c0756e9", force=True),
-            "ETH": accounts.at("0x7D24796f7dDB17d73e8B1d0A3bbD103FBA2cb2FE", force=True),
-            "cETH": accounts.at("0x1a1cd9c606727a7400bb2da6e4d5c70db5b4cade", force=True),
-            "NOTE": accounts.at("0x22341fB5D92D3d801144aA5A925F401A91418A05", force=True),
-            "COMP": accounts.at("0x7587cAefc8096f5F40ACB83A09Df031a018C66ec", force=True),
-        }
+        self.tokens = {}
+        for (symbol, addr) in addresses["tokens"].items():
+            if symbol.startswith("c"):
+                self.tokens[symbol] = Contract.from_abi(symbol, addr, cToken["abi"])
+            else:
+                self.tokens[symbol] = Contract.from_abi(symbol, addr, ERC20ABI)
 
-        self.deployer = accounts.at("0x8B64fA5Fd129df9c755eB82dB1e16D6D0Bdf5Bc3", force=True)
+        self.whales = {}
+        for (name, addr) in addresses["whales"].items():
+            self.whales[name] = accounts.at(addr, force=True)
+
+        self.deployer = accounts.at(addresses["deployer"], force=True)
         self.owner = accounts.at(self.notional.owner(), force=True)
 
 
-def getEnvironment():
-    return Environment()
+def getEnvironment(network = "mainnet"):
+    return Environment(network)
--- tests/test_wrapped_fcash.py
+++ tests/test_wrapped_fcash.py
@@ -2,9 +2,12 @@ import pytest
 import brownie
 import eth_abi
 from tests.helpers import get_balance_trade_action
-from brownie import Contract, WrappedfCash
-from brownie.network import chain
+from brownie import Contract, WrappedfCash, network
+from brownie.convert.datatypes import Wei
+from brownie.network import Chain
 from scripts.EnvironmentConfig import getEnvironment
+
+chain = Chain()
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
@@ -14,7 +17,11 @@ def run_around_tests():
 
 @pytest.fixture()
 def env():
-    return getEnvironment()
+    name = network.show_active()
+    if name == 'mainnet-fork':
+        return getEnvironment('mainnet')
+    elif name == 'kovan-fork':
+        return getEnvironment('kovan')
 
 @pytest.fixture() 
 def beacon(WrappedfCash, nUpgradeableBeacon, env):
@@ -92,12 +99,16 @@ def test_deploy_wrapped_fcash(factory, env):
     assert wrapper.name() == "Wrapped fDAI @ {}".format(markets[0][1])
     assert wrapper.symbol() == "wfDAI:{}".format(markets[0][1])
 
+def test_upgrade_wrapped_fcash(factory, env):
+    pass
+
 def test_cannot_deploy_wrapper_twice(factory, env):
     markets = env.notional.getActiveMarkets(2)
-    factory.deployWrapper(2, markets[0][1])
+    txn = factory.deployWrapper(2, markets[0][1])
+    assert txn.events['WrapperDeployed'] is not None
 
-    with brownie.reverts():
-        factory.deployWrapper(2, markets[0][1])
+    txn = factory.deployWrapper(2, markets[0][1])
+    assert 'WrapperDeployed' not in txn.events
 
 def test_cannot_deploy_invalid_currency(factory, env):
     markets = env.notional.getActiveMarkets(2)
@@ -166,7 +177,7 @@ def test_transfer_fcash(wrapper, lender, env):
 
     assert wrapper.balanceOf(lender) == 100_000e8
 
-def test_transfer_fcash_contract(wrapper, lender_contract, env):
+def test_transfer_fcash_to_contract(wrapper, lender_contract, env):
     env.notional.safeTransferFrom(
         lender_contract.address,
         wrapper.address,
@@ -191,9 +202,9 @@ def test_fail_redeem_above_balance(wrapper, lender, env):
     )
 
     with brownie.reverts():
-        wrapper.redeem(105_000e8, (False, False, lender.address), {"from": lender})
-        wrapper.redeemToAsset(105_000e8, lender.address, {"from": lender})
-        wrapper.redeemToUnderlying(105_000e8, lender.address, {"from": lender})
+        wrapper.redeem(105_000e8, (False, False, lender.address, 0), {"from": lender})
+        wrapper.redeemToAsset(105_000e8, lender.address, 0, {"from": lender})
+        wrapper.redeemToUnderlying(105_000e8, lender.address, 0, {"from": lender})
 
 def test_transfer_fcash(wrapper, lender, env):
     env.notional.safeTransferFrom(
@@ -204,11 +215,12 @@ def test_transfer_fcash(wrapper, lender, env):
         "",
         {"from": lender}
     )
-    wrapper.redeem(50_000e8, (False, True, lender), {"from": lender})
+    wrapper.redeem(50_000e8, (False, True, lender, 0), {"from": lender})
 
     assert wrapper.balanceOf(lender.address) == 50_000e8
     assert env.notional.balanceOf(lender.address, wrapper.getfCashId()) == 50_000e8
 
+@pytest.mark.skip
 def test_transfer_fcash_contract(wrapper, lender_contract, env):
     env.notional.safeTransferFrom(
         lender_contract.address,
@@ -219,10 +231,11 @@ def test_transfer_fcash_contract(wrapper, lender_contract, env):
         {"from": lender_contract}
     )
 
+    # This does not work on kovan right now...
     with brownie.reverts():
         wrapper.redeem(
             50_000e8,
-            (False, True, lender_contract),
+            (False, True, lender_contract, 0),
             {"from": lender_contract}
         )
 
@@ -242,10 +255,11 @@ def test_redeem_post_maturity_asset(wrapper, lender, env):
     )
 
     chain.mine(1, timestamp=wrapper.getMaturity())
-    wrapper.redeemToAsset(50_000e8, lender.address, {"from": lender})
+    wrapper.redeemToAsset(50_000e8, lender.address, 0, {"from": lender})
 
     assert wrapper.balanceOf(lender.address) == 50_000e8
-    assert env.tokens["cDAI"].balanceOf(lender.address) == 229452921165321
+    expectedAssetTokens = Wei(50_000e8 * 1e10 * 1e18) / env.tokens['cDAI'].exchangeRateStored()
+    assert pytest.approx(env.tokens["cDAI"].balanceOf(lender.address), abs=100) == expectedAssetTokens
 
 def test_redeem_post_maturity_underlying(wrapper, lender, env):
     env.notional.safeTransferFrom(
@@ -258,17 +272,60 @@ def test_redeem_post_maturity_underlying(wrapper, lender, env):
     )
 
     chain.mine(1, timestamp=wrapper.getMaturity())
-    wrapper.redeemToUnderlying(50_000e8, lender, {"from": lender})
+    wrapper.redeemToUnderlying(50_000e8, lender.address, 0, {"from": lender})
 
     assert wrapper.balanceOf(lender.address) == 50_000e8
     assert env.tokens["DAI"].balanceOf(lender.address) >= 50_000e18
 
-# Test fCash Trading
-def test_mint_and_redeem_fcash_via_underlying(wrapper, env):
+def test_redeem_failure_slippage(wrapper, lender, env):
+    env.notional.safeTransferFrom(
+        lender.address,
+        wrapper.address,
+        wrapper.getfCashId(),
+        100_000e8,
+        "",
+        {"from": lender}
+    )
+
+    with brownie.reverts('Trade failed, slippage'):
+        wrapper.redeemToUnderlying(50_000e8, lender.address, 0.01e9, {"from": lender})
+
+    wrapper.redeemToUnderlying(50_000e8, lender.address, 0.2e9, {"from": lender})
+    assert wrapper.balanceOf(lender.address) == 50_000e8
+
+# Test Direct fCash Trading
+def test_mint_failure_slippage(wrapper, lender, env):
     env.tokens["DAI"].approve(wrapper.address, 2 ** 255 - 1, {'from': env.whales["DAI_EOA"].address})
-    wrapper.mintFromUnderlying(
+    with brownie.reverts():
+        wrapper.mint(
+            10_000e18,
+            10_000e8,
+            env.whales["DAI_EOA"].address,
+            0.1e9,
+            True,
+            {'from': env.whales["DAI_EOA"].address}
+        )
+
+    wrapper.mint(
+        10_000e18,
         10_000e8,
         env.whales["DAI_EOA"].address,
+        0.01e9,
+        True,
+        {'from': env.whales["DAI_EOA"].address}
+    )
+
+    assert wrapper.balanceOf(env.whales["DAI_EOA"].address) == 10_000e8
+
+
+def test_mint_and_redeem_fcash_via_underlying(wrapper, env):
+    env.tokens["DAI"].approve(wrapper.address, 2 ** 255 - 1, {'from': env.whales["DAI_EOA"].address})
+    wrapper.mint(
+        10_000e18,
+        10_000e8,
+        env.whales["DAI_EOA"].address,
+        0,
+        True,
         {'from': env.whales["DAI_EOA"].address}
     )
 
@@ -284,6 +341,7 @@ def test_mint_and_redeem_fcash_via_underlying(wrapper, env):
     wrapper.redeemToUnderlying(
         10_000e8,
         env.whales["DAI_EOA"].address,
+        0,
         {"from": env.whales["DAI_EOA"].address}
     )
     balanceAfter = env.tokens["DAI"].balanceOf(env.whales["DAI_EOA"].address)
@@ -300,9 +358,12 @@ def test_mint_and_redeem_fusdc_via_underlying(factory, env):
     wrapper = Contract.from_abi("Wrapper", txn.events['WrapperDeployed']['wrapper'], WrappedfCash.abi)
 
     env.tokens["USDC"].approve(wrapper.address, 2 ** 255 - 1, {'from': env.whales["USDC"].address})
-    wrapper.mintFromUnderlying(
+    wrapper.mint(
+        10_000e6,
         10_000e8,
         env.whales["USDC"].address,
+        0,
+        True,
         {'from': env.whales["USDC"].address}
     )
 
@@ -318,6 +379,7 @@ def test_mint_and_redeem_fusdc_via_underlying(factory, env):
     wrapper.redeemToUnderlying(
         10_000e8,
         env.whales["USDC"].address,
+        0,
         {"from": env.whales["USDC"].address}
     )
     balanceAfter = env.tokens["USDC"].balanceOf(env.whales["USDC"].address)
@@ -330,9 +392,12 @@ def test_mint_and_redeem_fusdc_via_underlying(factory, env):
 
 def test_mint_and_redeem_fcash_via_asset(wrapper, env):
     env.tokens["cDAI"].approve(wrapper.address, 2 ** 255 - 1, {'from': env.whales["cDAI"].address})
-    wrapper.mintFromAsset(
+    wrapper.mint(
+        50_000e8,
         10_000e8,
         env.whales["cDAI"].address,
+        False,
+        0,
         {'from': env.whales["cDAI"].address}
     )
 
@@ -348,6 +413,7 @@ def test_mint_and_redeem_fcash_via_asset(wrapper, env):
     wrapper.redeemToAsset(
         10_000e8,
         env.whales["cDAI"].address,
+        0,
         {"from": env.whales["cDAI"].address}
     )
     balanceAfter = env.tokens["cDAI"].balanceOf(env.whales["cDAI"].address)
@@ -358,36 +424,97 @@ def test_mint_and_redeem_fcash_via_asset(wrapper, env):
     assert len(portfolio) == 0
     assert wrapper.balanceOf(env.whales["cDAI"].address) == 0
 
-def test_mint_and_redeem_feth_via_underlying(factory, env):
-    markets = env.notional.getActiveMarkets(1)
-    txn = factory.deployWrapper(1, markets[0][1])
-    wrapper = Contract.from_abi("Wrapper", txn.events['WrapperDeployed']['wrapper'], WrappedfCash.abi)
+# TODO: consider changing this to use WETH?
+# def test_mint_and_redeem_feth_via_ceth(factory, env):
+#     markets = env.notional.getActiveMarkets(1)
+#     txn = factory.deployWrapper(1, markets[0][1])
+#     wrapper = Contract.from_abi("Wrapper", txn.events['WrapperDeployed']['wrapper'], WrappedfCash.abi)
 
-    wrapper.mintFromUnderlying(
-        10e8,
-        env.whales["ETH_EOA"].address,
-        {'from': env.whales["ETH_EOA"].address, "value": 10e18}
+#     wrapper.mint(
+#         10e8,
+#         env.whales["ETH_EOA"].address,
+#         {'from': env.whales["ETH_EOA"].address, "value": 10e18}
+#     )
+
+#     assert wrapper.balanceOf(env.whales["ETH_EOA"].address) == 10e8
+#     assert wrapper.balance() == 0
+#     portfolio = env.notional.getAccount(wrapper.address)[2]
+#     assert portfolio[0][0] == wrapper.getCurrencyId()
+#     assert portfolio[0][1] == wrapper.getMaturity()
+#     assert portfolio[0][3] == 10e8
+#     assert len(portfolio) == 1
+
+#     # Now redeem the fCash
+#     balanceBefore = env.whales["ETH_EOA"].balance()
+#     wrapper.redeemToUnderlying(
+#         10e8,
+#         env.whales["ETH_EOA"].address,
+#         {"from": env.whales["ETH_EOA"].address}
+#     )
+#     balanceAfter = env.whales["ETH_EOA"].balance()
+#     balanceChange = balanceAfter - balanceBefore 
+
+#     assert 9.90e18 <= balanceChange and balanceChange <= 9.99e18
+#     portfolio = env.notional.getAccount(wrapper.address)[2]
+#     assert len(portfolio) == 0
+#     assert wrapper.balanceOf(env.whales["ETH_EOA"].address) == 0
+
+@pytest.mark.only
+def test_lend_via_erc1155_action(wrapper, env, accounts):
+    acct = accounts[0]
+    env.tokens["DAI"].transfer(acct, 100_000e18, {'from': env.whales["DAI_EOA"]})
+
+    env.tokens["DAI"].approve(env.notional.address, 2 ** 255 - 1, {'from': acct})
+    env.tokens["DAI"].approve(env.tokens["cDAI"].address, 2 ** 255 - 1, {'from': acct})
+
+    env.tokens["cDAI"].mint(100_000e18, {'from': acct})
+    env.tokens["cDAI"].approve(env.notional.address, 2**255-1, {'from': acct})
+
+    # Requires approval on the Notional side...
+    action = get_lend_action(
+        2,
+        [{"tradeActionType": "Lend", "marketIndex": wrapper.getMarketIndex(),
+            "notional": 100_000e8, "minSlippage": 0}],
+        False,
     )
+    lendCallData = env.notional.batchLend.encode_input(acct.address, [action])
 
-    assert wrapper.balanceOf(env.whales["ETH_EOA"].address) == 10e8
-    assert wrapper.balance() == 0
-    portfolio = env.notional.getAccount(wrapper.address)[2]
-    assert portfolio[0][0] == wrapper.getCurrencyId()
-    assert portfolio[0][1] == wrapper.getMaturity()
-    assert portfolio[0][3] == 10e8
-    assert len(portfolio) == 1
-
-    # Now redeem the fCash
-    balanceBefore = env.whales["ETH_EOA"].balance()
-    wrapper.redeemToUnderlying(
-        10e8,
-        env.whales["ETH_EOA"].address,
-        {"from": env.whales["ETH_EOA"].address}
+    # will msg.sender will lend directly on notional, via erc1155 transfer
+    env.notional.safeTransferFrom(
+        acct.address, # msg.sender
+        wrapper.address, # wrapper will receive fCash
+        wrapper.getfCashId(),
+        100_000e8,
+        lendCallData,
+        {"from": acct}
     )
-    balanceAfter = env.whales["ETH_EOA"].balance()
-    balanceChange = balanceAfter - balanceBefore 
+    # 488800261188499
+    assert False
 
-    assert 9.90e18 <= balanceChange and balanceChange <= 9.99e18
-    portfolio = env.notional.getAccount(wrapper.address)[2]
+    assert env.tokens["cDAI"].balanceOf(wrapper.address) == 0
+    assert env.tokens["DAI"].balanceOf(wrapper.address) == 0
+
+    # test balance on wrapper and in notional fCash
+    assert wrapper.balanceOf(acct.address) == 100_000e8
+    # assert that the account has no Notional position
+    portfolio = env.notional.getAccount(acct.address)[2]
     assert len(portfolio) == 0
-    assert wrapper.balanceOf(env.whales["ETH_EOA"].address) == 0
+
+def get_lend_action(currencyId, tradeActionData, depositUnderlying):
+    tradeActions = [get_trade_action(**t) for t in tradeActionData]
+    return (currencyId, depositUnderlying, tradeActions)
+
+def get_trade_action(**kwargs):
+    tradeActionType = kwargs["tradeActionType"]
+
+    if tradeActionType == "Lend":
+        return eth_abi.packed.encode_abi_packed(
+            ["uint8", "uint8", "uint88", "uint32", "uint120"],
+            [
+                0, # 0 == lend
+                kwargs["marketIndex"],
+                int(kwargs["notional"]),
+                int(kwargs["minSlippage"]),
+                0,
+            ],
+        )
